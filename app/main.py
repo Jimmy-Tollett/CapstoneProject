799b3536baa5d0be970db6a5645d80cc00e1fb72
Hunter37594 <hunter37594@gmail.com>
2025-09-12 14:21:59 -0500

    Create app directory with simple script to test Flask

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__) # Start web app
+
+# Check that she's actually alive
+@app.get("/healthz")
+def health():
+    return jsonify(ok=True)
+
+@app.get("/")
+def root():
+    return jsonify(service="smo-api", status="up")
