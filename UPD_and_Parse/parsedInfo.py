b57b8101b34df910d2635de0b7bd587bb787528f
MatthewDAgron <mdagron907@ou.edu>
2025-10-15 09:52:13 -0500

    Templates for all assign functions

--- UPD_and_Parse/parsedInfo.py
+++ UPD_and_Parse/parsedInfo.py
@@ -88,6 +88,227 @@ class ParsedInfo:
         self.ReservedExpansionField = ReservedExpansionField
         self.SpecialPurposeField = SpecialPurposeField
     
+def assignDataSource(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.dataSource = "TODO"
+    return
+
+def assignTargetReportDescriptor(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TargetReportDescriptor = "TODO"
+    return
+
+def assignTrackNumber(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TrackNumber = "TODO"
+    return
+
+def assignServiceID(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.ServiceID = "TODO"
+    return
+
+def assignTimeOfApplicabilityForPosition(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TimeofApplicabilityForPosition = "TODO"
+    return
+
+def assignPosWGS84(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.PosWGS84 = "TODO"
+    return
+
+def assignPosWGS84HighRes(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.posWGS84HighRes = "TODO"
+    return
+
+def assignTimeOfApplicVelocity(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TimeOfApplicVelocity = "TODO"
+    return
+
+def assignAirSpeed(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.AirSpeed = "TODO"
+    return
+
+def assignTrueAirSpeed(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TrueAirSpeed = "TODO"
+    return
+
+def assignTargetAddr(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TargetAddr = "TODO"
+    return
+
+def assignMessageReceptionOfPosTime(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.MessageReceptionOfPosTime = "TODO"
+    return
+
+def assignMessageReceptionOfPosTimeHighPres(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.MessageReceptionOfPosTimeHighPres = "TODO"
+    return
+
+def assignMessageReceptionofVel(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.MessageReceptionofVel = "TODO"
+    return
+
+def assignTimeMessageReceptionVelHighP(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TimeMessageReceptionVelHighP = "TODO"
+    return
+
+def assignGeometricHeight(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.GeometricHeight = "TODO"
+    return
+
+def assignQualityIndicators(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.GeometricHeight = "TODO"
+    return
+
+def assignMOPSVers(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.MOPSVers = "TODO"
+    return
+
+def assignMode3ACode(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.Mode3ACode = "TODO"
+    return
+
+def assignRollAngle(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.RollAngle = "TODO"
+    return
+
+def assignFlightLevel(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.FlightLevel = "TODO"
+    return
+
+def assignMagneticHeading(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.MagneticHeading = "TODO"
+    return
+
+def assignTargetStatus(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TargetStatus = "TODO"
+    return
+
+def assignBarometricVerticalRate(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.BarometricVerticalRate = "TODO"
+    return
+
+def assignGeometricVerticalRate(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.GeometricVerticalRate = "TODO"
+    return
+
+def assignAirborneGroundVector(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.GeometricVerticalRate = "TODO"
+    return
+
+def assignTrackAngleRate(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TrackAngleRate = "TODO"
+    return
+
+def assignTimeReportTransmission(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TrackAngleRate = "TODO"
+    return
+
+def assignTargetIdentification(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TargetIdentification = "TODO"
+    return
+
+def assignEmitterCategory(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.EmitterCategory = "TODO"
+    return
+
+def assignMetInformation(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.MetInformation = "TODO"
+    return
+
+def assignSelectedAltitude(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.SelectedAltitude = "TODO"
+    return
+
+def assignFinalStateSelectedAltitude(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.FinalStateSelectedAltitude = "TODO"
+    return
+
+def assignTrajIntent(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.TrajIntent = "TODO"
+    return
+
+def assignServManagement(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.ServManagement = "TODO"
+    return
+
+def assignAircraftOpStatus(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.AircraftOpStatus = "TODO"
+    return
+
+def assignSurfaceCapabilitiesAndCharacteristics(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.SurfaceCapabilitiesAndCharacteristics = "TODO"
+    return
+
+def assignMessageAmplitude(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.MessageAmplitude = "TODO"
+    return
+
+def assignBDSRegisterData(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.BDSRegisterData = "TODO"
+    return
+
+def assignACASResAdvReport(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.ACASResAdvReport = "TODO"
+    return
+
+def assignReceiverID(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.ReceiverID = "TODO"
+    return
+
+def assignDataAges(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.DataAges = "TODO"
+    return
+
+def assignReservedExpansionField(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.ReservedExpansionField = "TODO"
+    return
+
+def assignSpecialPurposeField(pushedInfo, assignTo: ParsedInfo):
+    # TODO
+    assignTo.SpecialPurposeField = "TODO"
+    return
+
+
 def parse(pushedInfo: str) -> ParsedInfo:
     for char in pushedInfo:
         print(".")
