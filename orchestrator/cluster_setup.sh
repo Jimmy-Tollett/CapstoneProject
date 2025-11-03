c891154af8f70dc00469065c3c3bb1d7e2063d1c
Hunter37594 <hunter37594@gmail.com>
2025-11-03 11:19:02 -0600

    Clean up root & expand orchestrator comments

--- orchestrator/cluster_setup.sh
+++ orchestrator/cluster_setup.sh
@@ -5,29 +5,32 @@ API_IMAGE="api-image:latest"
 PARSER_IMAGE="parser-image:latest"
 
 
-# This exposes the cluster's ingress/LoadBalancer port (80) to port 8080 on your host machine.
-# Your Electron client will talk to http://localhost:8080.
+# Create cluster (k3s streamlined w/ k3d)
 k3d cluster create ${CLUSTER_NAME} \
     --servers 1 \
     --agents 2 \
-    -p "8080:30080@agent:0"
+    -p "8080:30080@agent:0" # Exposes the API's NodePort (30080) to port 8080 on the host machine.
+
 
 # Build & import docker images
-# API container
+### API container
 cd ../backend
 docker build --no-cache -t ${API_IMAGE} .
 k3d image import ${API_IMAGE} -c ${CLUSTER_NAME}
 
-# Parser container
+### Parser container
 cd ../UPD_and_Parse
 docker build -t ${PARSER_IMAGE} .
 k3d image import ${PARSER_IMAGE} -c ${CLUSTER_NAME}
 
+
+# Apply .yaml files to pods
 cd ../orchestrator
 kubectl apply -f 01-database-pvc.yaml
 kubectl apply -f 02-database-deployment.yaml
 kubectl apply -f 03-api-deployment.yaml
 kubectl apply -f 04-parser-deployment.yaml
+
 
 # Tear down:
 # k3d cluster delete capstone
